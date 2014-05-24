3b25c76f7d0ba90a04f23bc5de7f9554efffddd1
Nitzan Shaked <nitzan.shaked@gmail.com>
2014-05-24 17:31:57 +0000

    added gzip tests

--- test/body-parser.js
+++ test/body-parser.js
@@ -1,15 +1,66 @@
 
 var assert = require('assert');
+var stream = require('stream');
+var zlib = require('zlib');
 var http = require('http');
 var request = require('supertest');
+var mikealRequest = require('request');
 
 var bodyParser = require('..');
 
 describe('bodyParser()', function(){
+
   var server;
   before(function(){
-    server = createServer()
+    server = createServer();
   })
+
+  testCompressed = function(contentEncoding, compressedStreamFactory, expectedStatusCode, expectedBody, done) {
+
+    server.listen(0);
+    var serverAddress = 'http://127.0.0.1:' + server.address().port;
+
+    var data = '{"user":"tobi"}';
+    var dataStream = new stream.Readable();
+    dataStream._read = function() {};
+    dataStream.push(data);
+    dataStream.push(null);
+
+    var requestOptions = {
+      url: serverAddress + '/',
+      headers: {
+        'content-type': 'application/json',
+        'content-encoding': contentEncoding,
+      },
+    };
+    var req = mikealRequest.post(requestOptions, function(err, response, body) {
+      if (err)
+        return done(err);
+      if (response.statusCode !== expectedStatusCode)
+        return done(new Error('invalid status code: expected ' + expectedStatusCode + ' got ' + response.statusCode));
+      if (expectedBody === null)
+        expectedBody = data;
+      if (body !== expectedBody)
+        return done(new Error('invalid response body: expected ' + expectedBody + ', got ' + response.body));
+      done();
+    });
+
+    dataStream
+      .pipe(compressedStreamFactory())
+      .pipe(req);
+  }
+
+  it('should support gzip', function(done){
+    testCompressed('gzip', zlib.createGzip, 200, null, done);
+  });
+
+  it('should support deflate', function(done){
+    testCompressed('deflate', zlib.createDeflate, 200, null, done);
+  });
+
+  it('should handle bad gzip', function(done){
+    testCompressed('gzip', stream.PassThrough, 500, 'incorrect header check', done);
+  });
 
   it('should default to {}', function(done){
     request(server)
